c362369f54eb89de13a9103808b6f4b376ef6b38
jspiner <jspiner@jspinerui-MacBook-Pro.local>
2017-08-26 19:20:20 +0900

    init project

---
+++ EduJoy/app/src/main/java/net/jspiner/edujoy/MainActivity.java
@@ -0,0 +1,19 @@
+package net.jspiner.edujoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+
+    }
+}
